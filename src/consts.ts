1e3a6de6e304492780803d3c8b0723537d490b7f
zzoce <zzoce9597@gmail.com>
2024-10-01 01:41:22 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,18 +2,18 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_TAB = "Frosti";
-export const SITE_TITLE = "Frosti 🧊";
+export const SITE_TAB = " zzoce";
+export const SITE_TITLE = "zzoce 🧊";
 export const SITE_DESCRIPTION = "A blog template";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"
+export const USER_NAME = "zzoce";
+export const USER_SITE = "https://www.zzoce.com/"
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
-export const SERVER_URL = "https://demo.saroprock.com";
+export const SERVER_URL = "https:// zzoce.com";
 
 // Menu items for navigation
 export const menuItems = [
